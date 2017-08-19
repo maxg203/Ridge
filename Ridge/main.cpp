fe89a3bcddcb056ea43e4b9943c7f3b32fd9dc01
Max Goodridge <max.goodridge@hotmail.co.uk>
2017-08-20 00:01:41 +0100

    Added first dependency include

--- Ridge/main.cpp
+++ Ridge/main.cpp
@@ -1,3 +1,4 @@
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 using namespace std;
@@ -9,6 +10,9 @@ int increment(int a, int b) {
 int main() {
 	cout << increment(2, 3) << endl;
 	cout << "Hello world!" << endl;
+	if (glfwInit()) {
+        cout << "glfw loaded!" << endl;
+    }
 
     // std::cin.get();
     return 0;
